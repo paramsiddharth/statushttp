4621f4b7d8f1f5250736d72f899b5c109491af1f
Param Siddharth <contact@paramsid.com>
2021-01-26 16:14:17 +0530

    Finish command-line version

--- statuscodes-cli.js
+++ statuscodes-cli.js
@@ -1,25 +1,59 @@
 #!/usr/bin/env node
+
 const yargs = require('yargs/yargs');
 const { hideBin } = require('yargs/helpers');
 const chalk = require('chalk');
-const { StatusCode, statusClass } = require('./statuscodes');
+const Table = require('cli-table');
+const { StatusCode, statusClass, statusText, statusDesc } = require('./statuscodes');
 const ack = chalk.greenBright(`Made with ❤ by Param Siddharth.`);
 
 if (require && require.main === module) {
-	var args = yargs(hideBin(process.argv))
+	var args = process.argv;
+	args = yargs(hideBin(args));
+	args = args
 		.usage(`\
 ${chalk.yellowBright('HTTP Status Codes (statuscodes)')}
 Gives details about HTTP status codes/messages.
 
 Usage: statuscodes <value> [options]`)
+		.check((argv, options) => {
+			if (!argv.help && !argv.version && !argv.list && argv._.length < 1)
+				throw 'Not enough non-option arguments: got 0, need at least 1';
+			else
+				return true;
+		})
+		.option('l', {
+			alias: 'list',
+			describe: 'Show all status codes',
+			default: false,
+			help: true,
+			boolean: true
+		})
 		.example('statuscodes 200', `Gives information about status code 200.`)
 		.example('statuscodes Ok', `Gives information about the HTTP status with message OK.`)
 		.example('statuscodes not found', `Gives information about the HTTP status with message 'Not Found'.`)
-		.demandCommand(1)
+		.alias('v', 'version')
 		.alias('?', 'help')
 		.help('?')
+		.recommendCommands()
+		.showHelpOnFail(true)
 		.epilog(ack)
 		.argv;
+	
+	if (args.list) {
+		const table = new Table({
+			head: ['Code', 'Message'].map(m => chalk.yellowBright(m)),
+			colors: 'yellow'
+		});
+		for (const code in statusText) {
+			table.push([
+				chalk.greenBright(code),
+				chalk.white(statusDesc[code])
+			]);
+		}
+		console.log(table.toString());
+		process.exit(0);
+	}
 	let value = args._[0];
 	if (args._.length > 1)
 		value = args._.join(' ');
@@ -27,7 +61,7 @@ Usage: statuscodes <value> [options]`)
 	try {
 		status = new StatusCode(value);
 	} catch(e) {
-		process.stderr.write(`${e.message}\n`);
+		process.stderr.write(`${chalk.redBright(e.message)}\n`);
 		process.exit(1);
 	}
 	process.stdout.write(`\
